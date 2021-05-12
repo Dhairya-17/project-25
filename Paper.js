f5295abf9720e06377cc353865169c5d576595c5
Dhairya-17 <80893400+Dhairya-17@users.noreply.github.com>
2021-05-12 12:54:50 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,36 @@
+class Paper{
+
+    constructor(x,y,r){
+var options={
+isStatic:false,
+restitution:0.3,
+friction:0,
+density:1.2
+}
+
+this.x=x;
+this.y=y;
+this.body=Bodies.circle(x,y,r,options)
+this.image=loadImage("paper.png")
+
+  World.add(world, this.body)
+    }
+
+    display(){
+		/*imageMode(CENTER)
+		image(this.body,0,0,this.bodywidth,this.bodyheight)*/
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			
+			//strokeWeight(4);
+			fill(128,128,128)
+			ellipse(0,0,this.r,this.r)
+			imageMode(CENTER);
+			image(this.image, 0,0,60, 50)
+			pop()
+			
+	}
+
+}
